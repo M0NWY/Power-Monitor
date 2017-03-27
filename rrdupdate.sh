a4e7977d00f4d6b702c73cb847df141e7ffb3f6a
Simon Newhouse <simon.newhouse@gmail.com>
2017-03-27 13:39:44 +0100

    Delete rrdupdate.sh

--- rrdupdate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#script to get values into rrd
-
-export SHELL=/bin/bash
-
-/usr/bin/rrdtool update /home/simon/shedometry/servertemps.rrd N:$(/home/simon/shedometry/servertemps.sh | tr '\r\n' ':' | rev | cut -c 2- | rev )
-
-#Daaamn! rev | cut | rev makes me feel dirty....
